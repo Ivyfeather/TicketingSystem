ffe6f6d0d7df01f797efddb60b76d5b9c37f43fd
Ivyfeather <chenxi171@mails.ucas.ac.cn>
2021-11-25 22:30:00 +0800

    little work

--- ticketingsystem/TicketingDS.java
+++ ticketingsystem/TicketingDS.java
@@ -1,7 +1,36 @@
 package ticketingsystem;
 
 public class TicketingDS implements TicketingSystem {
-		
-	//ToDo
+	private int routenum = 5;
+	private int coachnum = 8;
+	private int seatnum = 100;
+	private int stationnum = 10;
+	private int threadnum = 16;
 
+	public TicketingDS(int routenum, int coachnum, int seatnum, int stationnum, int threadnum) {
+		this.routenum = routenum;
+		this.coachnum = coachnum;
+		this.seatnum = seatnum;
+		this.stationnum = stationnum;
+		this.threadnum = threadnum;
+
+
+	}
+
+	
+	//ToDo
+	public Ticket buyTicket(String passenger, int route, int departure, int arrival){
+
+		return new Ticket();
+	}
+	
+	public int inquiry(int route, int departure, int arrival){
+		
+		return 1;
+	}
+
+	public boolean refundTicket(Ticket ticket){
+
+		return true;
+	}
 }
