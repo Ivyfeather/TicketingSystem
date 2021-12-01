8e6ce5641db99c36614ab673754c955579128f40
Ivyfeather <chenxi171@mails.ucas.ac.cn>
2021-12-01 12:46:42 +0800

    Update test file

--- test.sh
+++ test.sh
@@ -1,45 +1,8 @@
-#! /bin/sh
+#!/bin/sh
 
-rm logtest
-./clean.sh
-clear
 javac -encoding UTF-8 -cp . ticketingsystem/Test.java
-
-echo  "Thread: 8 testNum 10万" >> logtest
-java -cp . ticketingsystem/Test 8 100000 >> logtest
-
-echo  "Thread: 16 testNum 10万" >> logtest
-java -cp . ticketingsystem/Test 16 100000 >> logtest
-
-echo  "Thread: 32 testNum 10万" >> logtest
-java -cp . ticketingsystem/Test 32 100000 >> logtest
-clear
-cat logtest
-
-echo  "Thread: 64 testNum 10万" >> logtest
-java -cp . ticketingsystem/Test 64 100000 >> logtest
-clear
-cat logtest
-
-echo  "Thread: 8 testNum 100万" >> logtest
-java -cp . ticketingsystem/Test 8 1000000 >> logtest
-clear
-cat logtest
-
-echo  "Thread: 16 testNum 100万" >> logtest
-java -cp . ticketingsystem/Test 16 1000000 >> logtest
-clear
-cat logtest
-
-echo  "Thread: 32 testNum 100万" >> logtest
-java -cp . ticketingsystem/Test 32 1000000 >> logtest
-clear
-cat logtest
-
-echo  "Thread: 64 testNum 100万" >> logtest
-java -cp . ticketingsystem/Test 64 1000000 >> logtest
-clear
-cat logtest
-
-
-mv logtest donetest
+java -cp . ticketingsystem/Test 4 0
+java -cp . ticketingsystem/Test 8 0
+java -cp . ticketingsystem/Test 16 0
+java -cp . ticketingsystem/Test 32 0
+java -cp . ticketingsystem/Test 64 0
