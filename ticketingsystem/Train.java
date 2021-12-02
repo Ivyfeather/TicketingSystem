978a7ae8dd821568d0b3beb2314d1828332cf6de
Ivyfeather <chenxi171@mails.ucas.ac.cn>
2021-12-02 23:21:26 +0800

    Improve Inquiry - Big Advance

--- ticketingsystem/Train.java
+++ ticketingsystem/Train.java
@@ -1,101 +1,135 @@
 package ticketingsystem;
 
+import java.util.*;
 import java.util.BitSet;
 
-// class Seat {
-//     // !! public
-//     // public BitSet taken;
-//     BitSet taken;
+class Seat {
+    public BitSet taken;
 
-//     public Seat(int stationnum){
-//         taken = new BitSet(stationnum);
-//     }
+    public Seat(int stationnum){
+        taken = new BitSet(stationnum);
+    }
 
-//     public boolean checkAvail(int departure, int arrival){
-//         // return true;
-//         return taken.get(departure, arrival).isEmpty();
-//     }
+    public boolean checkAvail(int departure, int arrival){
+        return taken.get(departure, arrival).isEmpty();
+    }
 
-//     public void orderSeat(int departure, int arrival){
-//         // return;
-//         taken.set(departure, arrival);
-//     }
+    public void orderSeat(int departure, int arrival){
+        taken.set(departure, arrival);
+    }
 
-//     public void clearSeat(int departure, int arrival){
-//         // return;
-//         taken.clear(departure, arrival);
-//     }
+    public void clearSeat(int departure, int arrival){
+        taken.clear(departure, arrival);
+    }
     
-// }
+}
 
 class InquiryTable{
     int stationnum;
     int [][] c;
 
-    public InquiryTable(int stationnum){
+    public InquiryTable(int stationnum, int totalSeatnum){
+        this.stationnum = stationnum;
+
         c = new int[stationnum][stationnum];
-    }
-
-    public int inq(int departure, int arrival){
-        return c[departure][arrival];
-    }
-
-    public void buy(int departure, int arrival){
         int i,j;
-        for(i=0; i<arrival; i++){
-            for(j=departure+1; j<stationnum; j++){
+        for(i=0; i<stationnum; i++){
+            for(j=0; j<stationnum; j++){
+                c[i][j] = totalSeatnum;
+            }
+        }
+    }
+
+    public int inq(int dept, int arr){
+        // System.err.println("inq "+dept+" "+arr+" "+c[dept][arr]);
+        return c[dept][arr];
+    }
+
+    public void buy(int dept, int arr, int left, int right){
+        int i,j;
+        for(i=left; i<arr; i++){
+            for(j=dept+1; j<=right+1; j++){
                 c[i][j]--;
             }
         }
     }
 
-    public void refund(int departure, int arrival, int left, int right){
+    public void refund(int dept, int arr, int left, int right){
         int i,j;
-        
-        
+        for(i=left; i<arr; i++){
+            for(j=dept+1; j<=right+1; j++){
+                c[i][j]++;
+            }
+        }
     }
-
 
 }
 
 
 public class Train {
-    int route;
-    BitSet seats[];
+    int routeId;
     int coachnum = 8;
     int seatnum = 100;
+    int stationnum = 10;
+
+    Seat seats[];
+    InquiryTable inqTable;
 
     final boolean debug = false;
 
-    public Train(int route, int coachnum, int seatnum, int stationnum){
-        this.route = route;
+    public Train(int routeId, int coachnum, int seatnum, int stationnum){
+        this.routeId = routeId;
         this.coachnum = coachnum;
         this.seatnum = seatnum;
-        seats = new BitSet[coachnum*seatnum];
+        this.stationnum = stationnum;
+
+        seats = new Seat[coachnum*seatnum];
         for(int i=0; i<seats.length; i++){
-            seats[i] = new BitSet(stationnum);
+            seats[i] = new Seat(stationnum);
         }
+
+        inqTable = new InquiryTable(stationnum, coachnum*seatnum);
     }
 
     public int inquiry(int departure, int arrival){
-        int ticketsLeft = 0;
-        for(int i=0; i<seats.length; i++){
-            ticketsLeft += seats[i].get(departure, arrival).isEmpty()?1:0;
-        }
-        return ticketsLeft;
+        return inqTable.inq(departure, arrival);
     }    
 
     public Ticket buyTicket(int departure, int arrival){
-        for(int i=0; i<seats.length; i++){
-            boolean tmp = seats[i].get(departure, arrival).isEmpty();
+        if(inqTable.inq(departure, arrival) <= 0){
+            return null;
+        }
+
+        int length = seats.length;
+        for(int i=0; i<length; i++){
+            
+            boolean tmp = seats[i].checkAvail(departure, arrival);
             if(tmp){
-                seats[i].set(departure, arrival);
+                seats[i].orderSeat(departure, arrival);
                 Ticket t = new Ticket();
-                t.route = route + 1;
+                t.route = routeId + 1;
                 t.coach = (i/seatnum) + 1;
                 t.seat = (i%seatnum) + 1;
                 t.departure = departure + 1;
                 t.arrival = arrival + 1;
+                // System.err.println("HELLEN");
+                int left, right;
+
+
+                if(0 == departure){ 
+                    left = 0;
+                } else {
+                    left = seats[i].taken.previousSetBit(departure-1) + 1;
+                }
+
+                right = seats[i].taken.nextSetBit(arrival) - 1;
+                if(-2 == right) right = stationnum - 2;
+
+
+
+
+                inqTable.buy(departure, arrival, left, right);
+
                 return t;
             }
         }
@@ -104,10 +138,29 @@ public class Train {
     }  
 
     public void refundTicket(Ticket t){
-        // showSeatStatus();
 
         int sid = (t.coach-1)*seatnum + (t.seat-1);
-        seats[sid].clear(t.departure-1, t.arrival-1);
+        int dept = t.departure - 1;
+        int arr = t.arrival - 1;    
+
+        //!! left index out of boundary exception\
+        // returns -1 if no such set bit
+        int left, right;
+        if(0 == dept){ 
+            left = 0;
+        } else {
+            left = seats[sid].taken.previousSetBit(dept-1) + 1;
+        }
+
+        right = seats[sid].taken.nextSetBit(arr) - 1;
+        if(-2 == right) right = stationnum - 2;
+
+        inqTable.refund(dept, arr, left, right);
+        // System.err.println("WTLL");
+
+        seats[sid].clearSeat(dept, arr);
+
+        
     }
 
 
