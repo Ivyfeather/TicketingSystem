8c8d49852b9e8818477e39aa184ac23bcfdeb56e
Ivyfeather <chenxi171@mails.ucas.ac.cn>
2021-12-05 22:37:39 +0800

    passed Verification

--- ticketingsystem/Train.java
+++ ticketingsystem/Train.java
@@ -29,6 +29,7 @@ class Seat {
 
     public void clearSeat(int dept, int arr){
         int init = steps[arr];
+        if(-1 == init) init = 0;
         for(int i=arr-1; i>=0 && ( i>=dept || steps[i]!= -1); i--){
             steps[i] = ++init;
         }
@@ -37,15 +38,20 @@ class Seat {
     public int findLeft(int dept){
         if(0 == dept) return 0;
         int i;
-        for(i=dept-1; i>=0 && steps[i]!= -1; i--){}
-        return (i<0)? 0 : i+1;
+        for(i=dept-1; i>=0; i--){
+            if(-1 == steps[i]) return i+1;
+        }
+        return 0;
+
     }
     
     public int findRight(int arr){
         int stationnum = steps.length;
         int i;        
-        for(i=arr; i<stationnum && steps[i]!= -1; i++){}
-        return (i==stationnum)? stationnum-2 : i-1; 
+        for(i=arr; i<stationnum; i++){
+            if(-1 == steps[i]) return i-1;
+        }
+        return stationnum-2;
     }
 
 }
