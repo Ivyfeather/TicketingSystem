7d792394eb93408baf9a69742022e74dcc3e9431
Ivyfeather <chenxi171@mails.ucas.ac.cn>
2021-12-09 15:50:33 +0800

    add Replay Method; add train lock; remove check fake refund

--- ticketingsystem/TicketingDS.java
+++ ticketingsystem/TicketingDS.java
@@ -3,6 +3,7 @@ package ticketingsystem;
 import java.util.*;
 import java.util.concurrent.atomic.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.*;
 
 class SoldTickets{
     final ConcurrentHashMap<Long, Ticket> tickets = new ConcurrentHashMap<Long, Ticket>();
@@ -16,7 +17,10 @@ class SoldTickets{
     public boolean checkAndRemove(Ticket refund){
 		Ticket sold = tickets.get(refund.tid);
         // if first true, directly return false 
-        if( null == sold) return false;
+        if( null == sold){
+			System.err.println("Check Sold Empty");
+ 			return false;
+		}
 		if(!sold.passenger.equals(refund.passenger) ||
             sold.route != refund.route ||
             sold.coach != refund.coach ||
@@ -24,6 +28,9 @@ class SoldTickets{
             sold.departure != refund.departure ||
             sold.arrival != refund.arrival)
         {
+			System.err.println("Sold");		sold.print();
+			System.err.println("Refund");	refund.print();
+			System.err.println("Check Sold Mismatch");
             return false;
         }
 
@@ -36,16 +43,18 @@ class SoldTickets{
 public class TicketingDS implements TicketingSystem {
 	AtomicLong tid = new AtomicLong(1);
 	final SoldTickets sold = new SoldTickets();
+	// ReentrantLock trainLock[];
 
 	Train trains[];
     final boolean bypass = false;
 
 	public TicketingDS(int routenum, int coachnum, int seatnum, int stationnum, int threadnum) {
 		trains = new Train[routenum];
+		// trainLock = new ReentrantLock[routenum];
 		for(int i=0; i<trains.length; i++) {
 			trains[i] = new Train(i, coachnum, seatnum, stationnum);
+			// trainLock[i] = new ReentrantLock();
 		}
-
 	}
 
 
@@ -55,13 +64,18 @@ public class TicketingDS implements TicketingSystem {
 	// =================================
 	public Ticket buyTicket(String passenger, int route, int departure, int arrival){
 		if(bypass) return null;
+		// trainLock[route-1].lock();
+		
 		Ticket t = trains[route-1].buyTicket(departure-1, arrival-1);
 		if(t == null){
+			// trainLock[route-1].unlock();
 			return null;
 		}
 		t.tid = tid.getAndIncrement();
 		t.passenger = passenger;
-		sold.add(t);
+		// sold.add(t);
+		// trainLock[route-1].unlock();
+
 		return t;
 	}
 	
@@ -72,22 +86,24 @@ public class TicketingDS implements TicketingSystem {
 
 	public boolean refundTicket(Ticket t){
 		if(bypass) return true;
+		// trainLock[t.route-1].lock();
 
-		if(!sold.checkAndRemove(t)){
-			return false;
-		}
+		// if(!sold.checkAndRemove(t)){
+		// 	return false;
+		// }
 		trains[t.route-1].refundTicket(t);
+		// trainLock[t.route-1].unlock();
+
 		return true;
 	}
 
 	public boolean buyTicketReplay(Ticket t){
 		// buyTicket failed, so buyTicketReplay does nothing
-		if(0 == t.seat) return true;
-		return refundTicket(t);
+		if(null == t) return true;
+		return trains[t.route-1].buyTicketReplay(t);
 	}
 
 	public boolean refundTicketReplay(Ticket t){
-		sold.add(t);
 		return trains[t.route-1].refundTicketReplay(t);
 	}	
 }
